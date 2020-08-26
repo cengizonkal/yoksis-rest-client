0fcadc85e03cd5de95951f86ecbf2a55749f8277
cengiz <conkal@ciu.edu.tr>
2020-08-26 14:55:04 +0300

    Yerleştirme verisi fix json format

--- src/YOKSIS/REST/Resources/YerlestirmeVeri.php
+++ src/YOKSIS/REST/Resources/YerlestirmeVeri.php
@@ -4,13 +4,25 @@
 namespace Conkal\YOKSIS\REST\Resources;
 
 
-use Conkal\YOKSIS\REST\Resources\Traits\QueryTrait;
+
 
 class YerlestirmeVeri extends ResourceAbstract
 {
-    use QueryTrait;
+
 
     protected $endPoint = 'yerlestirmeveri';
     protected $entity = \Conkal\YOKSIS\REST\Entities\YerlestirmeVeri::class;
+
+    public function query(array $query)
+    {
+        $response = $this->client->send($this->endPoint, ['query' => $query])->content;
+        $entities = [];
+        if ($response) {
+            foreach ($response as $item) {
+                array_push($entities, new $this->entity($item));
+            }
+        }
+        return $entities;
+    }
 
 }
