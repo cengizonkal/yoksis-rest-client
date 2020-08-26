9e829b65f3ec5f7eb32b5956b7a5107324cf17a1
cengiz önkal <onkal.cengiz@gmail.com>
2020-08-26 21:11:14 +0300

    comments

--- src/YOKSIS/REST/Resources/YerlestirmeVeri.php
+++ src/YOKSIS/REST/Resources/YerlestirmeVeri.php
@@ -3,23 +3,26 @@
 
 namespace Conkal\YOKSIS\REST\Resources;
 
-
-
-
+/**
+ * Class YerlestirmeVeri
+ * @package Conkal\YOKSIS\REST\Resources
+ */
 class YerlestirmeVeri extends ResourceAbstract
 {
-
-
     protected $endPoint = 'yerlestirmeveri';
     protected $entity = \Conkal\YOKSIS\REST\Entities\YerlestirmeVeri::class;
 
+    /**
+     * @param array $query
+     * @return \Conkal\YOKSIS\REST\Entities\YerlestirmeVeri[]
+     */
     public function query(array $query)
     {
         $response = $this->client->send($this->endPoint, ['query' => $query])->content;
         $entities = [];
         if ($response) {
             foreach ($response as $item) {
-                array_push($entities, new $this->entity($item));
+                array_push($entities, new \Conkal\YOKSIS\REST\Entities\YerlestirmeVeri($item));
             }
         }
         return $entities;
